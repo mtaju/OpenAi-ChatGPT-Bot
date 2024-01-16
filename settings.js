2b5cf8a55cdf81005aa7dc625bdad59d6cda4c46
mtaju <152062507+mtaju@users.noreply.github.com>
2024-01-16 21:24:07 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ const chalk = require('chalk')
 🤖 𝗕𝗢𝗧 𝗦𝗘𝗧𝗧𝗜𝗡𝗚𝗦
 */
 global.OFFLINE = false // if you want put bot offline put true
-global.BOT_PRIVATE = true // if you want use bot privet 
+global.BOT_PRIVATE = false // if you want use bot privet 
 global.AUTO_CHATGPT = false // if you want auto ai bot mod
 global.READ_MESSAGE = false // if you want read message from bot
 global.ONLY_INBOX = false // Only inbox mod ( only pm ) use
@@ -16,12 +16,12 @@ global.ONLY_GROUPS = false // Only Groups Use
 /*
 🤖 𝗕𝗢𝗧 𝗜𝗡𝗙𝗢 𝗦𝗘𝗧𝗧𝗜𝗡𝗚𝗦
 */
-global.OWNER_NAME = 'Dark Maker Ofc' // Owner Name
-global.OWNER_NUMBER = ['94715166712'] //Owner Number
-global.BOT_ADMINS = ['94715166712','94715166712'] //Bot admins numbers Like a Sudo Numbers
-global.BLOCK_CHAT = [] // If you want Block Bot From Chats Use .jid Command and put Jids
+global.OWNER_NAME = 'LAZACK 28' // Owner Name
+global.OWNER_NUMBER = ['255734980103'] //Owner Number
+global.BOT_ADMINS = ['255734980103','255758868502'] //Bot admins numbers Like a Sudo Numbers
+global.BLOCK_CHAT = [.] // If you want Block Bot From Chats Use .jid Command and put Jids
 global.IMAGE_QUALITY = 'low' // Available Quality Are => low | mediam | high
-global.CAPTION = '*© 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 𝙾𝙿𝙴𝙽-𝙰𝙸(𝙲𝙷𝙰𝚃𝙶𝙿𝚃) 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿 𝙱𝙾𝚃*'
+global.CAPTION = '*@created by mr mtaju*'
 
 /*
 📌⭕ 𝗬𝗢𝗨 𝗠𝗨𝗦𝗧 𝗣𝗥𝗢𝗩𝗜𝗗𝗘 𝗕𝗘𝗙𝗢𝗥𝗘 𝗗𝗘𝗣𝗟𝗢𝗬 𝗕𝗢𝗧 📌⭕
@@ -40,7 +40,7 @@ global.IMAGE_CMD = 'aiimg' // .aiimg Command
 global.PORT = process.env.PORT || '5000' // Port No need Change.
 
 mess = {
-	owner: '*This Command is only For Owner.*',
+	owner: '*This command is for mr mtaju only.*',
 	need_text: '*Please Give Some words*'
 }
 react = {
